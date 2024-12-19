cc246d6ad842eb593aa829d8b7cbd4afb5c7e955
Sarvesh Ingale <smi1081@mavs.uta.edu>
2024-12-18 21:00:51 -0500

    Added constructor and fields for Line

---
+++ P02/full_credit/Line.java
@@ -0,0 +1,19 @@
+
+public class Line {
+
+    public Line(double x1, double y1, double x2, double y2, Color color) {
+
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.color = color;        
+    }
+
+
+    private double x1;
+    private double x2;
+    private double y1;
+    private double y2;
+    private Color color;
+}
