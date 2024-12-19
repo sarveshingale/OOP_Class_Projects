cdfe76bb3976de92d7bb4e3658a1822370dae119
Sarvesh Ingale <smi1081@mavs.uta.edu>
2024-12-19 10:20:06 -0500

    Completed Bonus

--- P02/bonus/Color.java
+++ P02/bonus/Color.java
@@ -1,4 +1,17 @@
 
 public enum Color {
-    LIME, PINK, TEAL, ORANGE;
+    LIME(0X32CD32), PINK(0XFFC0CB), TEAL(0X008080), ORANGE(0XFFA500);
+	
+	private Color(int rgb) {
+		
+		this.rgb = rgb;
+	}
+	
+	@Override
+	public String toString() {
+		return name() + " (0x" + String.format("%x", rgb) + ")";
+	}
+	
+	private int rgb;
+		
 }
