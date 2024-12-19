75bc7d83c0a5d3f043186dfe0245ae6dc3e2aa19
Sarvesh Ingale <smi1081@mavs.uta.edu>
2024-12-19 15:16:07 -0500

    Added main for lms

--- P03/full_credit/LibraryManager.java
+++ P03/full_credit/LibraryManager.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
 public class LibraryManager {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		
+		Library library = new Library("UTA Central Library");
+		//System.out.println(library);
+		Publication book1 = new Publication("Harry Potter", "JK Rowling", 2001);
+		library.addPublication(book1);
+		Publication book2 = new Publication("Geronimo Stilton", "Mouse", 2014);
+		library.addPublication(book2);
+		Publication book3 = new Publication("Lord of the Rings", "R.R Tolken", 2002);
+		library.addPublication(book3);
+		System.out.println(library);
+		System.out.println("\n\nWhich book would you like to check out (Provide index): ");
+		Scanner sc = new Scanner(System.in);
+		int index = sc.nextInt();
+		System.out.println("Who are you? ");
+		System.out.println(library);		
 	}
 }
