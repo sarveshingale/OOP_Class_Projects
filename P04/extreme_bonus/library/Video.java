eda17c6614a639ec4724c5fdd3e280a2c77741b5
Sarvesh Ingale <smi1081@mavs.uta.edu>
2024-12-20 19:52:38 -0500

    Completed extreme_bonus

--- P04/extreme_bonus/library/Video.java
+++ P04/extreme_bonus/library/Video.java
@@ -1,9 +1,23 @@
 package library;
 import java.time.Duration;
-// Creating sublass Video by inheriting from Publication
+
+/**
+ * This class models a Video
+ *
+ * @author Sarvesh Ingale
+ * @version 1.0
+ * @since 1.0
+ */
 
 public class Video extends Publication {
-		
+	
+	/**
+	 * Custom exception for invalid runtime
+	 *
+	 * @author Sarvesh Ingale
+	 * @version 1.0
+	 * @since 1.0
+	 */
 	public class InvalidRuntimeException extends ArithmeticException {
 		public InvalidRuntimeException() {
 			super();
@@ -15,6 +29,13 @@ public class Video extends Publication {
 			super(String.format("%s has invalid runtime %d", title, runtime));		
 		}
 	}
+	/**
+	 * Instances Video object
+	 *
+	 * @param title Title of the video.
+	 * @param author Author of the video.
+	 * @param runtime Duration of the video
+	 */
 	public Video(String title, String author, int copyright, int runtime) {
 		super(title, author, copyright);
 		if(runtime <= 0) {
@@ -23,6 +44,11 @@ public class Video extends Publication {
 		this.runtime = Duration.ofMinutes(runtime);
 	}
 	
+	/**
+	 * Overriden toString method that calls on toStringBuilder
+	 *
+	 * @return Formatted String obtained from StringBuilder
+	 */
 	@Override
 	public String toString() {
 		
