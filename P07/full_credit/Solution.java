cd66fe8e72fcd370dbcc36caba7e8888e6e98379
Sarvesh Ingale <smi1081@mavs.uta.edu>
2024-12-29 09:53:55 -0500

    Solutions are now sorted

--- P07/full_credit/Solution.java
+++ P07/full_credit/Solution.java
@@ -1,4 +1,4 @@
-public class Solution { 
+public class Solution implements Comparable<Solution> { 
     public Solution(String name, String word, int x, int y, Direction direction) {
         this.name = name;
         this.word = word;
@@ -10,7 +10,15 @@ public class Solution {
     public String toString() {
         return String.format("In %s: %s found at (%d,%d,%s)", name, word, x, y, direction);
     }
-
+	
+	@Override public int compareTo(Solution solution) {
+		int result = this.name.compareTo(solution.name);
+		if(result == 0) {
+			result = this.word.compareTo(solution.word);
+		}
+		return result;
+	}
+	
     private String name;
     private String word;
     private int x;
