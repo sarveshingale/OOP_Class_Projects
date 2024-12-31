1cfa768b1309b0e8863e6ac4447573fa74149bab
Sarvesh Ingale <smi1081@mavs.uta.edu>
2024-12-31 11:10:21 -0500

    Completed full_credit

---
+++ P08/full_credit/hello_all.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+	
+	std::string name;
+	std::cout << "What is your name? ";
+	std::getline(std::cin, name);
+	std::cout << "Hello, " << name << "!" << std::endl;
+	return 0;
+}
