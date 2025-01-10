686a214861ddb41627692b3fe494ed83d9494782
Sarvesh Ingale <smi1081@mavs.uta.edu>
2025-01-10 10:15:42 -0500

    Added logic for parsing lines with sstream

--- P11/full_credit/mkindex.cpp
+++ P11/full_credit/mkindex.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <sstream>
 #include "Index.h"
 
 int main(int argc, char* argv[]) {
 	
 	
-	std::vector<std::string> words{}; // Stores a line in the file
+	std::string file_line; // Stores a line in file
+	std::string word; // Stores a word from iss
 	Index index{};
 	for(int i = 1; i < argc; i++) {
 		std::ifstream ifts{argv[i]};
@@ -21,21 +23,28 @@ int main(int argc, char* argv[]) {
 		}
 		int line = 1;
 		while(ifts) {
-			std::getline(ifts, words, ' ');
-			for(auto& word : words) {
-				for(auto& c : word) {
-					if(!isalpha(c)) {
-						c = ' ';
-					}
-					if(isupper(c)) {
-						c = tolower(c);
-					}
-				}
-				index.add_word(word, argv[i], line);
+			std::getline(ifts, file_line);
+			std::istringstream iss{file_line};
+			
+			iss >> word;
+			if(!isalpha(word.front())) {
+				word.erase(0, 1);
 			}
+			if(!isalpha(word.back())) {
+				word.pop_back();
+			}
+			
+			for(auto& c : word) {				
+				if(isupper(c)) {
+					c = tolower(c);
+				}
+			}
+			
 			line++;
 		}
 	}
+	
+	std::cout << index << std::endl;
 			
 	return 0;
 }
